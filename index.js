f329dbfc88127d5c8c7db9206b943120e52c84c2
christlaye24 <christlaye24@gmail.com>
2025-06-10 09:35:51 +0200

    Initial commit: Setup basic Express API

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur l\'API Node.js basique !');
+});
+
+app.listen(PORT, () => {
+    console.log(`API démarrée sur le port ${PORT}`);
+});
